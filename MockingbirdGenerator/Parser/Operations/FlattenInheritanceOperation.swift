285ee75bc08cc5902e3b9110dd0853afab3900d1
Andrew Chang <andrew.chang@bird.co>
2019-09-15 22:04:43 -0700

    Refactor `ProcessTypesOperation.swift` for readability

---
+++ MockingbirdGenerator/Parser/Operations/FlattenInheritanceOperation.swift
@@ -0,0 +1,120 @@
+//
+//  FlattenInheritanceOperation.swift
+//  MockingbirdGenerator
+//
+//  Created by Andrew Chang on 9/15/19.
+//
+
+import Foundation
+import SourceKittenFramework
+
+/// Given a set of `RawType` partials for a single type, creates a `MockableType` object by going to
+/// the top of the type inheritance tree and walking downwards. Each step flattens the tree and
+/// memoizes a `MockableType` object. Once the process reaches the bottom original type, all
+/// inherited dependency types exist and can be merged. See the `MockableType` initializer for the
+/// merging strategy.
+class FlattenInheritanceOperation: BasicOperation {
+  let rawType: [RawType]
+  let moduleDependencies: [String: Set<String>]
+  let rawTypeRepository: RawTypeRepository
+  let typealiasRepository: TypealiasRepository
+  
+  class Result {
+    fileprivate(set) var mockableType: MockableType?
+  }
+  
+  let result = Result()
+  
+  init(rawType: [RawType],
+       moduleDependencies: [String: Set<String>],
+       rawTypeRepository: RawTypeRepository,
+       typealiasRepository: TypealiasRepository) {
+    precondition(!rawType.isEmpty)
+    self.rawType = rawType
+    self.moduleDependencies = moduleDependencies
+    self.rawTypeRepository = rawTypeRepository
+    self.typealiasRepository = typealiasRepository
+  }
+  
+  override func run() throws {
+    // Module names are put into an array and sorted so that looking up types is deterministic.
+    let moduleNames = Array(Set(rawType.flatMap({
+      $0.parsedFile.importedModuleNames.flatMap({ moduleDependencies[$0] ?? [$0] })
+        + [$0.parsedFile.moduleName]
+    }))).sorted()
+    result.mockableType = flattenInheritance(for: rawType, moduleNames: moduleNames)
+  }
+  
+  /// Recursively traverse the inheritance graph from bottom up (children to parents). Note that
+  /// `rawType` is actually an unmerged set of all `RawType` declarations found eg in extensions.
+  private static var memoizedMockbleTypes = Synchronized<[String: MockableType]>([:])
+  private func flattenInheritance(for rawType: [RawType], moduleNames: [String]) -> MockableType? {
+    // Create a copy of `memoizedMockableTypes` to reduce lock contention.
+    let memoizedMockableTypes = FlattenInheritanceOperation.memoizedMockbleTypes.value
+    guard let baseRawType = rawType.findBaseRawType() else { return nil }
+    
+    let fullyQualifiedName = baseRawType.fullyQualifiedModuleName
+    if let memoized = memoizedMockableTypes[fullyQualifiedName] { return memoized }
+    
+    let rawTypeRepository = self.rawTypeRepository
+    let typealiasRepository = self.typealiasRepository
+    let createMockableType: (Bool) -> MockableType? = { hasOpaqueInheritedType in
+      // Flattening inherited types could have updated `memoizedMockableTypes`.
+      var memoizedMockableTypes = FlattenInheritanceOperation.memoizedMockbleTypes.value
+      let mockableType = MockableType(from: rawType,
+                                      mockableTypes: memoizedMockableTypes,
+                                      hasOpaqueInheritedType: hasOpaqueInheritedType,
+                                      moduleNames: moduleNames,
+                                      rawTypeRepository: rawTypeRepository,
+                                      typealiasRepository: typealiasRepository)
+      // Contained types can inherit from their containing types, so store store this potentially
+      // preliminary result first.
+      FlattenInheritanceOperation.memoizedMockbleTypes.update {
+        $0[fullyQualifiedName] = mockableType
+      }
+      
+      let containedTypes = rawType.flatMap({ $0.containedTypes })
+      guard !containedTypes.isEmpty else { return mockableType } // No contained types, early out.
+      
+      // For each contained type, flatten it before adding it to `mockableType`.
+      memoizedMockableTypes[fullyQualifiedName] = mockableType
+      mockableType?.containedTypes = containedTypes.compactMap({
+        self.flattenInheritance(for: [$0], moduleNames: moduleNames)
+      })
+      FlattenInheritanceOperation.memoizedMockbleTypes.update {
+        $0[fullyQualifiedName] = mockableType
+      }
+      return mockableType
+    }
+    
+    // Base case where the type doesn't inherit from anything.
+    let inheritedTypes = rawType
+      .compactMap({ $0.dictionary[SwiftDocKey.inheritedtypes.rawValue] as? [StructureDictionary] })
+      .flatMap({ $0 })
+    guard !inheritedTypes.isEmpty else { return createMockableType(false) }
+    
+    var hasOpaqueInheritedType = false
+    let rawInheritedTypes = inheritedTypes
+      .compactMap({ $0[SwiftDocKey.name.rawValue] as? String }) // Get type name.
+      .compactMap({ typeName -> [RawType]? in // Get stored raw type.
+        let nearest = rawTypeRepository
+          .nearestInheritedType(named: typeName,
+                                moduleNames: moduleNames,
+                                referencingModuleName: baseRawType.parsedFile.moduleName,
+                                containingTypeNames: baseRawType.containingTypeNames[...])
+        if nearest == nil {
+          fputs("Missing source for referenced type `\(typeName)` in \(baseRawType.parsedFile.path.absolute())\n", stderr)
+          hasOpaqueInheritedType = true
+        }
+        return nearest
+      })
+      .flatMap({ $0 })
+    
+    // If there are inherited types that aren't processed, flatten them first.
+    if rawInheritedTypes.filter({ memoizedMockableTypes[$0.fullyQualifiedModuleName] == nil }).count > 0 {
+      rawInheritedTypes.forEach({ _ = flattenInheritance(for: [$0], moduleNames: moduleNames) })
+    }
+    
+    return createMockableType(hasOpaqueInheritedType)
+  }
+}
---
+++ MockingbirdGenerator/Parser/Operations/ProcessStructuresOperation.swift
@@ -0,0 +1,77 @@
+//
+//  ProcessStructuresOperation.swift
+//  MockingbirdGenerator
+//
+//  Created by Andrew Chang on 9/15/19.
+//
+
+import Foundation
+import SourceKittenFramework
+
+/// Creates minimal `RawType` partial objects from `ParsedFile` SourceKitten structures. Partials
+/// can be classes, protocols, extensions, typealiases, etc, which are then combined later in a
+/// `FlattenInheritanceOperation` to create a hydrated `MockableType`.
+class ProcessStructuresOperation: BasicOperation {
+  let structureDictionary: StructureDictionary
+  let parsedFile: ParsedFile
+  
+  class Result {
+    fileprivate(set) var rawTypes = [RawType]()
+  }
+  
+  let result = Result()
+  
+  init(structureDictionary: StructureDictionary, parsedFile: ParsedFile) {
+    self.structureDictionary = structureDictionary
+    self.parsedFile = parsedFile
+  }
+  
+  override func run() throws {
+    result.rawTypes.append(contentsOf: processStructureDictionary(structureDictionary,
+                                                                  parsedFile: parsedFile,
+                                                                  containingTypeNames: []))
+  }
+  
+  /// Create a `RawType` object from a parsed file's `StructureDictionary`.
+  private func processStructureDictionary(_ dictionary: StructureDictionary,
+                                          parsedFile: ParsedFile,
+                                          containingTypeNames: [String]) -> [RawType] {
+    let typeName = dictionary[SwiftDocKey.name.rawValue] as? String
+    
+    let substructure = dictionary[SwiftDocKey.substructure.rawValue] as? [StructureDictionary] ?? []
+    let genericTypes = substructure.compactMap({ dictionary -> String? in
+      guard let kind = SwiftDeclarationKind(from: dictionary), kind == .genericTypeParam
+        else { return nil }
+      return dictionary[SwiftDocKey.name.rawValue] as? String
+    })
+    let allGenericTypes = genericTypes.isEmpty ? "" : "<\(genericTypes.joined(separator: ", "))>"
+    
+    let attributedContainingTypeNames: [String] // Containing types plus the current type.
+    if let name = typeName {
+      attributedContainingTypeNames = containingTypeNames + [name + allGenericTypes]
+    } else {
+      attributedContainingTypeNames = containingTypeNames
+    }
+    
+    let containedTypes = substructure.flatMap({
+      processStructureDictionary($0,
+                                 parsedFile: parsedFile,
+                                 containingTypeNames: attributedContainingTypeNames)
+    })
+    guard let name = typeName else { return containedTypes } // Base case where this isn't a type.
+    
+    // For inheritance, contained types are stored in the root namespace as fully qualified types.
+    containedTypes.forEach({ result.rawTypes.append($0) })
+    
+    guard let kind = SwiftDeclarationKind(from: dictionary), kind.isParsable,
+      let accessLevel = AccessLevel(from: dictionary), accessLevel.isMockable else { return [] }
+    let fullyQualifiedName = attributedContainingTypeNames.joined(separator: ".")
+    return [RawType(dictionary: dictionary,
+                    name: name,
+                    fullyQualifiedName: fullyQualifiedName,
+                    containedTypes: containedTypes,
+                    containingTypeNames: containingTypeNames,
+                    kind: kind,
+                    parsedFile: parsedFile)]
+  }
+}
