90699678bccd149f5772108c668914f506dfbce2
Andrew Chang <andrew.chang@bird.co>
2019-09-16 13:27:33 -0700

    Clean up `TypeFacade` communication and value passing

--- MockingbirdFramework/DSL/TypeFacade.swift
+++ MockingbirdFramework/DSL/TypeFacade.swift
@@ -7,34 +7,37 @@
 
 import Foundation
 
-// This is a hack to get strongly-typed stubbing/verification parameters. The goal is to have
-// `ArgumentMatcher` "conform" to any reference or value type so that it's possible to pass both
-// an actual concrete instance of a type OR a matcher. The method provides stronger compile-time
-// guarantees and better autocomplete compared to simply conforming parameter types to a common
-// protocol such as `Matchable`.
-//
-// It goes without saying that this should probably never be done in production.
-private class TypeFacade {
+/// This is a hack to get strongly-typed stubbing/verification parameters. The goal is to have
+/// `ArgumentMatcher` "conform" to any reference or value type so that it's possible to pass both
+/// an actual concrete instance of a type OR a matcher. The method provides stronger compile-time
+/// guarantees and better autocomplete compared to simply conforming parameter types to a common
+/// protocol such as `Matchable`.
+///
+/// It goes without saying that this should probably never be done in production.
+class TypeFacade {
   static let shared = TypeFacade()
-  static let threadValueKey = "value"
-  static let threadDidSetValueKey = "didset-value"
-  static let threadScopeKey = DispatchSpecificKey<String>()
+  
+  class StoredValue {
+    var value: Any? {
+      didSet { isSet = true }
+    }
+    private(set) var isSet = false
+  }
+  static let storedValueKey = DispatchSpecificKey<StoredValue>()
 }
 
 struct AnyObjectFake {}
 
 extension DispatchQueue {
-  class var threadScope: String? {
-    return DispatchQueue.getSpecific(key: TypeFacade.threadScopeKey)
+  class var storedValue: TypeFacade.StoredValue? {
+    return DispatchQueue.getSpecific(key: TypeFacade.storedValueKey)
   }
 }
 
+/// Wraps a value into any type `T`.
 func createTypeFacade<T>(_ value: Any?) -> T {
-  // We can't use the casted TypeFacade directly, so we store the desired wrapped value on the heap.
-  if let threadScope = DispatchQueue.threadScope {
-    Thread.current.threadDictionary["\(threadScope)-\(TypeFacade.threadDidSetValueKey)"] = true
-    Thread.current.threadDictionary["\(threadScope)-\(TypeFacade.threadValueKey)"] = value
-  }
+  // We can't return `value` directly, so we return it using the KV store on `DispatchQueue`.
+  if let storedValue = DispatchQueue.storedValue { storedValue.value = value }
   
   // Trivial case where `T` is a non-nominal type such as `Any` or `AnyObject`.
   if let concreteType = AnyObjectFake() as? T { return concreteType }
@@ -44,18 +47,15 @@ func createTypeFacade<T>(_ value: Any?) -> T {
     .pointee
 }
 
+/// Resolve `parameter` when `T` is _not_ known to be `Equatable`.
 func resolve<T>(_ parameter: @escaping () -> T) -> ArgumentMatcher {
-  let scope = UUID().uuidString
-  let scopedDidSetValueKey = "\(scope)-\(TypeFacade.threadDidSetValueKey)"
-  let scopedValueKey = "\(scope)-\(TypeFacade.threadValueKey)"
-  
+  let storedValue = TypeFacade.StoredValue()
   let queue = DispatchQueue(label: "co.bird.mockingbird.typefacade")
-  queue.setSpecific(key: TypeFacade.threadScopeKey, value: scope)
-  
+  queue.setSpecific(key: TypeFacade.storedValueKey, value: storedValue)
   var resolvedMatcher: ArgumentMatcher!
   queue.sync {
     let realValue = parameter() // It's only safe to store this on the stack.
-    guard Thread.current.threadDictionary[scopedDidSetValueKey] as? Bool == true else {
+    guard storedValue.isSet else { // The closure contained a concrete `T` instance.
       if let matcher = realValue as? ArgumentMatcher {
         resolvedMatcher = matcher // `realValue` is already an `ArgumentMatcher`.
       } else {
@@ -63,25 +63,21 @@ func resolve<T>(_ parameter: @escaping () -> T) -> ArgumentMatcher {
       }
       return
     }
-    // Use the wrapped value returned by resolving type facade.
-    resolvedMatcher = Thread.current.threadDictionary[scopedValueKey] as? ArgumentMatcher
+    // Use the wrapped value returned by resolving the type facade closure.
+    resolvedMatcher = storedValue.value as? ArgumentMatcher
   }
   return resolvedMatcher
 }
 
-// When `T` is known to be equatable at compile time.
+/// Resolve `parameter` when `T` is known to be `Equatable`.
 func resolve<T: Equatable>(_ parameter: @escaping () -> T) -> ArgumentMatcher {
-  let scope = UUID().uuidString
-  let scopedDidSetValueKey = "\(scope)-\(TypeFacade.threadDidSetValueKey)"
-  let scopedValueKey = "\(scope)-\(TypeFacade.threadValueKey)"
-  
+  let storedValue = TypeFacade.StoredValue()
   let queue = DispatchQueue(label: "co.bird.mockingbird.typefacade")
-  queue.setSpecific(key: TypeFacade.threadScopeKey, value: scope)
-  
+  queue.setSpecific(key: TypeFacade.storedValueKey, value: storedValue)
   var resolvedMatcher: ArgumentMatcher!
   queue.sync {
     let realValue = parameter() // It's only safe to store this on the stack.
-    guard Thread.current.threadDictionary[scopedDidSetValueKey] as? Bool == true else {
+    guard storedValue.isSet else { // The closure contained a concrete `T` instance.
       if let matcher = realValue as? ArgumentMatcher {
         resolvedMatcher = matcher // `realValue` is already an `ArgumentMatcher`.
       } else {
@@ -89,13 +85,13 @@ func resolve<T: Equatable>(_ parameter: @escaping () -> T) -> ArgumentMatcher {
       }
       return
     }
-    // Use the wrapped value returned by resolving type facade.
-    resolvedMatcher = Thread.current.threadDictionary[scopedValueKey] as? ArgumentMatcher
+    // Use the wrapped value returned by resolving the type facade closure.
+    resolvedMatcher = storedValue.value as? ArgumentMatcher
   }
   return resolvedMatcher
 }
 
-// When the compiler knows the closure returns an ArgumentMatcher.
+/// Resolve `parameter` when the closure returns an `ArgumentMatcher`.
 func resolve(_ parameter: @escaping () -> ArgumentMatcher) -> ArgumentMatcher {
   return parameter()
 }
