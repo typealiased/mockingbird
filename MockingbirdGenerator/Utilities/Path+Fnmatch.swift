d26ab5dcc3c95b3497f8517b9dd467e7361c1496
Andrew Chang <47129469+andrewchang-bird@users.noreply.github.com>
2019-10-29 16:43:22 -0700

    Fix fnmatch implementation on macOS Catalina (#15)

--- MockingbirdGenerator/Utilities/Path+Fnmatch.swift
+++ MockingbirdGenerator/Utilities/Path+Fnmatch.swift
@@ -16,15 +16,12 @@ extension Path {
   ///   - isDirectory: Whether this path is a directory, if known at call time.
   /// - Returns: True if the path matches, false otherwise.
   func matches(pattern: String, isDirectory: Bool? = nil) -> Bool {
-    // PathKit strips trailing slashes when normalizing which causes directory globs to fail.
-    let isDirectory = isDirectory ?? self.isDirectory
-    
-    guard let rawPattern = pattern.data(using: .utf8, allowLossyConversion: false)?.withUnsafeBytes({
-      return $0.bindMemory(to: Int8.self).baseAddress
-    }), let rawPathString = "\(self)\(isDirectory ? "/" : "")".data(using: .utf8, allowLossyConversion: false)?.withUnsafeBytes({
-      return $0.bindMemory(to: Int8.self).baseAddress
-    }) else { return false }
-    
-    return fnmatch(rawPattern, rawPathString, 0) == 0
+    // PathKit strips trailing slashes for normalized paths which can cause directory globs to fail.
+    let rawPathString = "\(self)"
+    let shouldAppendTrailingSlash = !rawPathString.hasSuffix("/")
+      && pattern.hasSuffix("/") // Only apply trailing slash fix-it to directory globs.
+      && (isDirectory ?? self.isDirectory)
+    let pathString = rawPathString + (shouldAppendTrailingSlash ? "/" : "")
+    return fnmatch(pattern, pathString, FNM_PATHNAME | FNM_PERIOD) != FNM_NOMATCH
   }
 }
