3b1636c5128e1eb0f59eb3d9cde0a1faa304169c
Andrew Chang <47129469+andrewchang-bird@users.noreply.github.com>
2020-03-03 09:46:05 -0800

    Add Custom[Debug]StringConvertible to standard supporting source files (#51)

--- MockingbirdSupport/Swift/Misc.swift
+++ MockingbirdSupport/Swift/Misc.swift
@@ -3,3 +3,11 @@ public protocol AnyObject {}
 public typealias `class` = AnyObject
 
 public typealias AnyClass = AnyObject.Type
+
+public protocol CustomStringConvertible {
+  var description: String { get }
+}
+
+public protocol CustomDebugStringConvertible {
+  var debugDescription: String { get }
+}
