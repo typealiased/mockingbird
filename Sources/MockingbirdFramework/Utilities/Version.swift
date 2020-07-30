59be6fbb5a3ebacf60a3ebbb806fdaa17547309b
Andrew Chang <typealiased@gmail.com>
2020-07-30 00:59:02 -0700

    Bump versions to 0.14.1

--- Sources/MockingbirdFramework/Utilities/Version.swift
+++ Sources/MockingbirdFramework/Utilities/Version.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 /// Swift Package Manager doesn't create an Info.plist section in the binary like xcodebuild.
-let mockingbirdVersion = Version(shortString: "0.14.0")
+let mockingbirdVersion = Version(shortString: "0.14.1")
 
 struct Version: Comparable, CustomStringConvertible {
   let semver: [Int]
--- Sources/MockingbirdGenerator/Utilities/Version.swift
+++ Sources/MockingbirdGenerator/Utilities/Version.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 /// Swift Package Manager doesn't create an Info.plist section in the binary like xcodebuild.
-public let mockingbirdVersion = Version(shortString: "0.14.0")
+public let mockingbirdVersion = Version(shortString: "0.14.1")
 
 public struct Version: Comparable, CustomStringConvertible {
   let semver: [Int]
