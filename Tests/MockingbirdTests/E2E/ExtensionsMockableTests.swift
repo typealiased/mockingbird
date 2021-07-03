14af898d2e5128874f990efac81de86e1510dbc0
Andrew Chang <typealiased@gmail.com>
2021-07-02 17:19:11 -0700

    Remove mocking methods in protocol extensions
    Although methods defined in protocol extensions can be re-defined in a
    conforming type, static dispatch means that the original method will
    always be called. Currently the generator creates mocking code for
    protocol extensions which was leading to unexpected results in tests
    for stubbing and verification.

--- Tests/MockingbirdTests/E2E/ExtensionsMockableTests.swift
+++ Tests/MockingbirdTests/E2E/ExtensionsMockableTests.swift
@@ -8,19 +8,19 @@
 
 import Foundation
 import Mockingbird
-import MockingbirdTestsHost
+@testable import MockingbirdTestsHost
 
 // MARK: - Mockable declarations
 
 private protocol MockableExtendableProtocol: Mock {
   func trivialBaseMethod()
   var baseVariable: Bool { get }
-  
+
   func trivialExtendedMethod()
   func parameterizedExtendedMethod(param1: Bool)
   func parameterizedReturningExtendedMethod(param1: Bool) -> Bool
   var extendedVariable: Bool { get }
-  
+
   func anotherTrivialExtendedMethod()
   var anotherExtendedVariable: Bool { get }
 }
@@ -37,15 +37,3 @@ private protocol MockableNonExtendableClass: Mock {
   var baseVariable: Bool { get }
 }
 extension NonExtendableClassMock: MockableNonExtendableClass {}
-
-// MARK: - Non-mockable declarations
-
-private extension NonExtendableClassMock {
-  func trivialExtendedMethod() {}
-  func parameterizedExtendedMethod(param1: Bool) {}
-  func parameterizedReturningExtendedMethod(param1: Bool) -> Bool { return true }
-  var extendedVariable: Bool { return true }
-  
-  // MARK: Encodable
-  func encode(to encoder: Encoder) throws {}
-}
