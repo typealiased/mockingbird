3e1615016a942a944ade3bfa3ec9b7a26137196e
Andrew Chang <47129469+andrewchang-bird@users.noreply.github.com>
2021-07-08 13:08:05 -0700

    Bump Swift Syntax version to 5.4 for Xcode 12.5 (#204)

--- Sources/Package.swift
+++ Sources/Package.swift
@@ -16,7 +16,7 @@ let package = Package(
   // Keep this in sync with the Mockingbird.xcodeproj dependencies!
   dependencies: [
     .package(url: "https://github.com/apple/swift-package-manager.git", .exact("0.4.0")),
-    .package(url: "https://github.com/apple/swift-syntax.git", .exact("0.50300.0")),
+    .package(url: "https://github.com/apple/swift-syntax.git", .exact("0.50400.0")),
     .package(url: "https://github.com/jpsim/SourceKitten.git", .exact("0.30.0")),
     .package(url: "https://github.com/tuist/XcodeProj.git", .exact("7.14.0")),
     .package(url: "https://github.com/weichsel/ZIPFoundation.git", .exact("0.9.11")),
