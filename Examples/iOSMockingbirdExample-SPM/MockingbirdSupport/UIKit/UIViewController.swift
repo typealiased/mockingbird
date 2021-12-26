c39f04ad5e09f2e9a611be716515881a49260035
Andrew Chang <typealiased@gmail.com>
2021-12-25 21:02:47 -1000

    Remove legacy SwiftPM example project

--- Examples/iOSMockingbirdExample-SPM/MockingbirdSupport/UIKit/UIViewController.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-import UIKit
-
-@available(iOS 2.0, *)
-open class UIViewController : UIResponder, NSCoding {
-  public init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?)
-  public required init?(coder: NSCoder)
-}
