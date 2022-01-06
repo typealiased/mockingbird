2513e29630ef119e78fd3ff3ecb479bbf3decf50
Andrew Chang <47129469+andrewchang-bird@users.noreply.github.com>
2022-01-05 20:18:14 -1000

    Fix unavailable generic protocol mock initializer (#251)
    Mock initializer functions were not migrated to use the new templates in
    0.18. This caused unavailable generic protocols (e.g. inheriting from an
    opaque external type) to generate malformed output.

--- Sources/MockingbirdGenerator/Generator/Templates/Primitives/FunctionDefinitionTemplate.swift
+++ Sources/MockingbirdGenerator/Generator/Templates/Primitives/FunctionDefinitionTemplate.swift
@@ -8,15 +8,18 @@
 import Foundation
 
 struct FunctionDefinitionTemplate: Template {
+  let leadingTrivia: String
   let attributes: [String]
   let declaration: String
   let genericConstraints: [String]
   let body: String
   
-  init(attributes: [String] = [],
+  init(leadingTrivia: String = "",
+       attributes: [String] = [],
        declaration: String,
        genericConstraints: [String] = [],
        body: String) {
+    self.leadingTrivia = leadingTrivia
     self.attributes = attributes
     self.declaration = declaration
     self.genericConstraints = genericConstraints
@@ -27,6 +30,7 @@ struct FunctionDefinitionTemplate: Template {
     let genericConstraintsString = genericConstraints.isEmpty ? "" :
       " where \(separated: genericConstraints)"
     return String(lines: [
+      leadingTrivia,
       attributes.filter({ !$0.isEmpty }).joined(separator: " "),
       declaration + genericConstraintsString + " " + BlockTemplate(body: body).render()
     ])
