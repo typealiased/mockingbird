64f95eb73cde671ef24305f05b818cbca5cc8ede
Andrew Chang <typealiased@gmail.com>
2022-01-07 03:34:36 -1000

    Bump versions to 0.19.0

--- Sources/MockingbirdCommon/Version.swift
+++ Sources/MockingbirdCommon/Version.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-public let mockingbirdVersion = Version(shortString: "0.18.0")
+public let mockingbirdVersion = Version(shortString: "0.19.0")
 
 public struct Version: Comparable, CustomStringConvertible {
   public let semver: [Int]
