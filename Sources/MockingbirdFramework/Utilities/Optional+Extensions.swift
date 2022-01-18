5dea192938a942cb5771edac86b42c9260707c02
Andrew Chang <typealias@fb.com>
2022-01-18 15:02:13 -0800

    Remove Obj-C based nil check

---
+++ Sources/MockingbirdFramework/Utilities/Optional+Extensions.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+protocol Nullable {
+  var isNil: Bool { get }
+}
+
+/// Used to determine if a type-erased value is `nil`.
+extension Optional: Nullable {
+  /// Whether the wrapped value is equal to `nil`.
+  var isNil: Bool { self == nil }
+}
