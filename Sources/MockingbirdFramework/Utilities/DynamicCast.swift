9f7ba48e770f82ea60add10da998c00a890cd75f
Andrew Chang <typealias@fb.com>
2022-01-18 15:38:04 -0800

    Fix nested optional properties and Obj-C

--- Sources/MockingbirdFramework/Utilities/DynamicCast.swift
+++ Sources/MockingbirdFramework/Utilities/DynamicCast.swift
@@ -2,6 +2,6 @@ import Foundation
 
 /// Swift versions before 5.4 cannot cast from `Any` to a more optional type.
 /// https://forums.swift.org/t/casting-from-any-to-optional/21883
-func dynamicCast<T>(_ value: Any?) -> T {
+func dynamicCast<T>(_ value: Any) -> T {
   return value as! T
 }
